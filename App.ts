487e608e3036969213c61ac6bd78e345482c910a
Sanni-Damilola <sannifortune11@gmail.com>
2023-04-02 15:28:04 +0100

    updated

--- App.ts
+++ App.ts
@@ -1,9 +1,23 @@
 /** @format */
 
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 
 export const AppConfig = (app: Application) => {
-  app.use(express.json()).use(cors()).use(morgan("dev"));
+  app
+    .use(express.json())
+    .use(cors())
+    .use(morgan("dev"))
+
+    .all("*", (req: Request, res: Response) => {
+      res.status(404).json({
+        message: `routes not found ${req.originalUrl}`,
+      });
+    })
+    .get("/", (req: Request, res: Response) => {
+      res.status(201).json({
+        message: "Server is up and running",
+      });
+    });
 };
